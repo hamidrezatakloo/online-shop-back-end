31311ecdfc4222fbaf7debbdb341adfe609a199a
takloo <takloohamidreza@gmail.com>
2024-09-03 22:49:57 +0330

    :Sparkles: Feat: Add apps and set User model in settings

--- configs/settings.py
+++ configs/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+AUTH_USER_MODEL = 'users.User'
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.users',
+    'apps.products',
+    'apps.orders',
+    'apps.locations',
 ]
 
 MIDDLEWARE = [
