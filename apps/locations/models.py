c0f120c569a0995ff298ca04b592f650cec6f711
takloo <takloohamidreza@gmail.com>
2024-09-03 22:54:40 +0330

    :sparkles: Feat: create models and make migrations for location app

--- apps/locations/models.py
+++ apps/locations/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Address(models.Model):
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='addresses')
+    city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='addresses')
+    street = models.CharField(max_length=255)
+    postal_code = models.CharField(max_length=10)
+    is_default = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.street}, {self.city}'
